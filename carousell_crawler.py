ab6d587aa18a5766224541e04cb05c6edfd3bbe6
jean <jeanlin0703@gmail.com>
2025-06-16 15:26:40 +0800

    Second Commit

--- carousell_crawler.py
+++ carousell_crawler.py
@@ -8,30 +8,37 @@ from chinese_converter import converter
 
 def carousell_crawler():
     options = Options()
-    options.add_experimental_option("detach", True)  # Keep browser open
+    options.add_experimental_option("detach", True)
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-    url = "https://tw.carousell.com/categories/women-s-fashion-4/"
-    driver.get(url)
+    try:
+        url = "https://tw.carousell.com/categories/women-s-fashion-4/"
+        driver.get(url)
 
-    input("The browser is open. Click 'Show more results' as many times as you'd like. Press ENTER here when done...\n")
+        input("Click 'Show more results' button on the page as many time as you want.\nPress ENTER when finish......\n")
 
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
-    products = soup.select("div.D_ps")
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
+        products = soup.select("div.D_ps")
 
-    output = []
-    for product in products:
-        traditional = product.select_one('p.D_jg.D_ju').get_text()
-        price = product.select_one('p.D_jt').get_text().split('(')[0].strip()
-        simplified = converter(traditional)
-        output.append([traditional, simplified, price])
+        output = []
+        for product in products:
+            try:
+                traditional = product.select_one('p.D_jg.D_ju').get_text()
+                price = product.select_one('p.D_jt').get_text().split('(')[0].strip()
+                simplified = converter(traditional)
+                output.append([traditional, simplified, price])
+            except Exception:
+                print("A product has some issues.(Skip)")
+                continue
 
-    with open('output.csv', 'w', newline='', encoding='utf-8') as myfile:
-        wr = csv.writer(myfile)
-        wr.writerow(["Traditional Chinese", "Simplified Chinese", "Price"])
-        wr.writerows(output)
+        with open('output.csv', 'w', newline='', encoding='utf-8') as myfile:
+            wr = csv.writer(myfile)
+            wr.writerow(["Traditional Chinese", "Simplified Chinese", "Price"])
+            wr.writerows(output)
 
-    print("Successfully save to output!")
-
-    #print("Can't load the website.")
+        print("Successfully save to output!")
+    except Exception:
+        print("Can't load the website (or accidently closs the page before press ENTER).")
+    finally:
+        driver.quit()
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from carousell_crawler import carousell_crawler
 
 if __name__ == "__main__":
-
     carousell_crawler()
